24ae4af766e1873c658d3e75ae1e6618b64b7254
Marcus <marcus.kalmar@gmail.com>
2019-11-25 11:31:56 +0100

    Fix dotenv config and add bot template

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const slackbot = require('slackbots')
+const SlackBot = require('slackbots')
 const axios = require('axios')
+const dotenv = require('dotenv')
 
+dotenv.config()
+
+const bot = new SlackBot({
+    token: `${process.env.SLACKBOT_API_TOKEN}`,
+    name: 'megatron'
+})
